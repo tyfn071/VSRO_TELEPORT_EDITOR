8604b9c821dd556b2f381b3a436237368c375b06
tyfn071 <tayfunosman.071@gmail.com>
2020-07-11 00:07:38 +0300

    SqlObjects

--- VSRO_TELEPORT_EDITOR/Globals/CTeleportBase.cs
+++ VSRO_TELEPORT_EDITOR/Globals/CTeleportBase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace VSRO_TELEPORT_EDITOR
 {
@@ -19,6 +20,12 @@ namespace VSRO_TELEPORT_EDITOR
         }
         public EditStatus m_Status;
         public int m_Service { get; set; }
+        protected SqlCommand m_SqlObj;
+        protected abstract string GetInsertQuery();
+        protected abstract string GetUpdateQuery();
+        protected abstract string GetRemoveQuery();
+        protected abstract void LoadParameters();
+
 
         public virtual void SaveToDatabase()
         {
---
+++ VSRO_TELEPORT_EDITOR/Globals/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSRO_TELEPORT_EDITOR
+{
+    static class Globals
+    {
+        internal static string s_SqlConnectionString;
+    }
+}
